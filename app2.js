4389c1670f0e6fb3463a6b0240906be722e971dd
Milos Zivanovic <mixy80@gmail.com>
2018-02-27 03:26:54 +0100

    radicaly changed code (added features) and style, also responsive now

--- app2.js
+++ app2.js
@@ -44,8 +44,7 @@
 
                              <App selectionHandler={this.handleTableRowClick} data={this.state.data} />
                         </div>
-                        <div className="details"><div className="scroll">
-                        <UserDetails user={this.state.selectedUser}/></div></div>
+                       
 
                     </div>
                 );
@@ -117,7 +116,11 @@ class Article extends React.Component {
 
     constructor(props){
       super(props)
-      this.state = {data: {}};
+      this.state = {data: {}, style: {display:'none'}, h2: {height:''},
+                     h2: {height:''}, 
+                              details:{det:'Click for details!'},
+                              color:{backgroundColor: '#eee', color: 'blue'}};
+                    
       this.onSuccess = this.onSuccess.bind(this)
 
     }
@@ -136,11 +139,42 @@ class Article extends React.Component {
                   this.props.clickHandler(this.props.item);
             }
 
+   
+
+  buttonClick() {
+   
+    if (this.state.style.display === "none" ) {
+            this.state.style.display = " ";
+       
+            this.state.h2.height='auto';
+             this.state.details.det='***Hide details!***';
+            this.state.color.backgroundColor='#ca4646';
+            this.state.color.color='white';
+
+        }       
+
+  else {
+            this.state.style.display = "none";
+         
+              this.state.h2.height='';
+              this.state.details.det='Click for details!';
+              this.state.color.backgroundColor='#eee';
+              this.state.color.color='blue';
+
+        }
+
+ 
+
+}
+
+
+
+
     componentDidMount(){
 
         $.ajax({
-          url: `https://api.linkpreview.net/?key=5a8c6f397e808b560f6b4a8e44ba5736d6ff6aa644f25&q=${this.props.url}`,
-         /* url: 'https://api.linkpreview.net/?key=123456&q=https://www.google.com',*/
+          /*url: `https://api.linkpreview.net/?key=5a8c6f397e808b560f6b4a8e44ba5736d6ff6aa644f25&q=${this.props.url}`,*/
+          url: 'https://api.linkpreview.net/?key=123456&q=https://www.google.com',
           success: this.onSuccess
         })
 
@@ -148,18 +182,43 @@ class Article extends React.Component {
 
     
     render(){
+
+   
+
+
      /* console.log(this.state.data.url);
        console.log(this.state.data);*/
          const user = this.props.item;
         /* console.log(user);*/
         const clickHandler = this.props.clickHandler;
-      return (
-        <div className="flexi" onClick={() => clickHandler(user)}>
+      return (  
+        <div className="flexi" style= {{height:this.state.h2.height}} onClick={() => clickHandler(user)}>
+             <div id="wrap">
+              <div id="wraptext">
           <h3 >{this.state.data.title}</h3><br/>
           <p >{this.state.data.description}</p><br/>
+              </div>
           <img  className="aa" id="imm" src={this.state.data.image} ></img>
-         
+        <button className="collapsible"onClick={this.buttonClick.bind(this)} style={{backgroundColor: this.state.color.backgroundColor, color: this.state.color.color}}>{this.state.details.det}</button><br/>
+             </div>
+        <div className="content" style={{display:this.state.style.display}} >
+        <div>
+              <ul >
+                       
+                        <li>Source: {user.source}</li>
+                        <li>Section name: {user.section_name}</li>
+                        <li>Published: {user.pub_date}</li>
+                        <li>Snippet: {user.snippet}</li>
+                        <li>URL:<a target="_blank" href= {user.web_url}>{user.web_url}</a></li> 
+                        
+                                                
+                       
+                        
+                    </ul>
         </div>
+        </div>
+        </div>
+
       );
     }
 }
